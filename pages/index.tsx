b33874b2627e4b7e538c284bc6182f7d1fbd3940
Huynh Duc Dung <dunghd.it@gmail.com>
2022-05-08 15:14:17 +0800

    feat: add image from rainbowkit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Head from "next/head";
+import Image from "next/image";
 
 import styles from "../styles/Home.module.css";
 
@@ -21,13 +22,9 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js</a> +{" "}
           <a href="https://www.rainbowkit.com/">RainbowKit!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+        <h3>RainbowKit - The best way to connect a wallet 🌈</h3>
         <ConnectButton />
+        <Image src={"/images/rainbowkit.webp"} width={650} height={320} />
       </main>
     </div>
   );
